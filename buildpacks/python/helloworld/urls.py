3e5e7a1475c5c5fa4edf9bcf297aa9008c796aae
duanhongyi <duanhongyi@users.noreply.github.com>
2025-07-22 13:38:58 +0800

    fix(django): url not found error

--- buildpacks/python/helloworld/urls.py
+++ buildpacks/python/helloworld/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^healthz', views.healthz, name='healthz'),
-    url(r'^letters', views.letters, name='letters'),
-    url(r'^$', views.index, name='index'),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^healthz', views.healthz, name='healthz'),
+    re_path(r'^letters', views.letters, name='letters'),
+    re_path(r'^$', views.index, name='index'),
 ]
